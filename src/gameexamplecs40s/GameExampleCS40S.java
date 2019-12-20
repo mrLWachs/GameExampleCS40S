241b01c8db037da15266cdde9376852347e676da
Mr. Wachs <mrLWachs@gmail.com>
2019-12-19 18:09:26 -0600

    The in-class examples

--- src/gameexamplecs40s/GameExampleCS40S.java
+++ src/gameexamplecs40s/GameExampleCS40S.java
@@ -1,17 +1,22 @@
 
-
+/** required package class namespace */
 package gameexamplecs40s;
 
 
 /**
- * GameExampleCS40S.java - 
+ * GameExampleCS40S.java - an advanced gaming example that could be 
+ * adapted into a final project. It uses advanced code, but code at the 
+ * level learned in this course.
  *
  * @author Mr. Wachs 
  * @since 19-Dec-2019 
  */ 
-public class GameExampleCS40S {
+public class GameExampleCS40S 
+{
 
     /**
+     * Main method for the project
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
